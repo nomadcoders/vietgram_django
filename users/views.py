f5cd139ae7962234770ea48647ddf8e3dba52449
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-05 13:11:30 +0700

    Steps for David

--- users/views.py
+++ users/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
-# 1) Create the views
-# 2) Create the urls with the views
-# 3) Test with admin user and anon user
+# 1) Render the template with render function (line one)
+# 2) Create a folder named 'static' and another named 'templates'
+# 3) Add STATIC_DIRS to settings.py (look on github)
+# 4) Modifie TEMPLATE DIRS on settings.py (look on github)
+# 5) Add template tags to your html (look on github)
 
 
 def index(request):
