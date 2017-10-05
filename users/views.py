4f7b02322a5bdf190a4fea7d29cb01f81f0b9db2
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-05 11:34:33 +0700

    Steps

--- users/views.py
+++ users/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
+# 1) Create the views
+# 2) Create the urls with the views
+# 3) Test with admin user and anon user
+
 
 def index(request):
     if request.user.is_authenticated():
