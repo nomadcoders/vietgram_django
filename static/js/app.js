8cd3bf9c8e1ba00414b435a1790829bbc6c61e39
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-06 18:31:47 +0700

    Clickin heart

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-  var $heart = $("#feedHeart");
+  var $heart = $(".feedHeart");
   $heart.click(function() {
-    console.log("heart clicked");
+    var imageId = $(this).attr("data-id");
+    // if ($(this).hasClass("fa-heart-o")) {
+    //   $(this).removeClass("fa-heart-o");
+    //   $(this).addClass("fa-heart clickedHeart");
+    // } else {
+    //   $(this).removeClass("fa-heart clickedHeart");
+    //   $(this).addClass("fa-heart-o");
+    // }
+    $(this).toggleClass("fa-heart-o fa-heart clickedHeart");
   });
 });
