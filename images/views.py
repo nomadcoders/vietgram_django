571510a66235f46775fde73af3b018d2184e00d9
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-06 16:36:39 +0700

    Liking photo

--- images/views.py
+++ images/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from . import models
+from django.http import HttpResponse
 # Create your views here.
 
 
 def like_image(request, image_id):
 
-    # 0) Create a url like: '/images/{ID_OF_IMAGE}/like/
-    # 1) Find a Like that exists with the image_id and the user_id
-    # 2) If the like doesnt exist, create a like
-    # 3) If the like is create return an HttpResponse with the status of 200
-    # 4) If it already exists don't creatie the like and return  an HttpResponse with the status of 400
+    try:
+        existing_like = models.Like.objects.get(
+            image__id=image_id, user__id=request.user.id)
+        existing_like.delete()
+        response = HttpResponse(status=202)
+    except models.Like.DoesNotExist:
+        image = models.Image.objects.get(id=image_id)
+        new_like = models.Like.objects.create(
+            user=request.user,
+            image=image
+        )
+        new_like.save()
+        response = HttpResponse(status=200)
+
+    return response
--- vietgram/urls.py
+++ vietgram/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
+from images import views as image_views
 
 urlpatterns = [
+
     url(
         regex=r'^$',
         view=user_views.index,
@@ -21,6 +23,11 @@ urlpatterns = [
         name='explore'
     ),
     url(r'^nicoladmin/', admin.site.urls),
+    url(
+        regex=r'^images/(?P<image_id>[\d+])/like/$',
+        view=image_views.like_image,
+        name='like_image'
+    ),
     url(
         regex=r'^(?P<username_from_url>.+)/$',
         view=user_views.profile,
